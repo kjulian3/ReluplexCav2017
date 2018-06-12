63de7a6f7f90f74f41eab4feab43ecdac8105ba1
Kyle Julian <kjulian3@stanford.edu>
2018-06-11 18:31:15 -0700

    Updating example

--- reluplexpy/examples/example3.py
+++ reluplexpy/examples/example3.py
@@ -1,7 +1,9 @@
 import numpy as np
-from maraboupy import Marabou
+from reluplexpy import Reluplex
 
 tf_file_name = "./networks/graph_test_medium.pb"
-net2 = Marabou.read_tf(tf_file_name)
-net2.setLowerBound(net2.outputVars.item(0),-3.0)
-net2.solve("")
+net2 = Reluplex.read_tf(tf_file_name)
+print("With Reluplex:")
+print(net2.evaluateWithReluplex(np.ones(net2.inputVars.shape))[0])
+print("With Tensorflow:")
+print(net2.evaluateWithoutReluplex(np.ones(net2.inputVars.shape)))
